e2fa98eeb14aea1de56a627f1ca997f9aeb7a6af
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-05-26 23:59:29 -0500

    Revert "✨ feat: Login"
    This reverts commit 566ce1a4cf7ffa886147ff5652905db0e107ac39.

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from "../views/HomeView.vue";
 import Users from "../views/Users.vue";
 import NewUser from "@/components/users/NewUser.vue";
 import EditUser from "@/components/users/EditUser.vue";
-import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
@@ -11,12 +10,10 @@ const routes = [
     name: "home",
     component: HomeView,
   },
-
   {
     path: "/users",
     name: "Users",
     component: Users,
-    meta: { requiresAdmin: true }
   },
   {
     path: "/users/new",
@@ -27,11 +24,6 @@ const routes = [
     path: "/users/:id/edit",
     name: "EditarUsuario",
     component: EditUser,
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
   },
   {
     path: "/about",
@@ -49,27 +41,4 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  const isAuthenticated = !!user;
-
-  // Rutas públicas (login, etc)
-  const publicPages = ['login', 'about'];
-  const authRequired = !publicPages.includes(to.name);
-
-  if (authRequired && !isAuthenticated) {
-    return next({ name: 'login' });
-  }
-
-  // Ejemplo rol (si tienes rol en user)
-  if (to.meta.requiresAdmin && (!user || !user.isAdmin)) {
-    return next({ name: 'home' }); // o página "no autorizado"
-  }
-
-  if (to.name === 'login' && isAuthenticated) {
-    return next({ name: 'home' });
-  }
-
-  next();
-});
 export default router;
